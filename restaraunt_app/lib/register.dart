f141107fc83b6135829bc52d2f371f3c60442972
Saenthuran Vignarajah <55508775+SaenthuranVigna@users.noreply.github.com>
2021-11-14 14:40:44 -0500

    Added page routes for login/register

---
+++ restaraunt_app/lib/register.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class register extends StatelessWidget {
+  const register({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Register"),),
+      
+    );
+  }
+}
