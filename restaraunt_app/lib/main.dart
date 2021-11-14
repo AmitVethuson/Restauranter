fdfe60e69d102b0c095e76bcc0f612ba3793549a
Saenthuran Vignarajah <55508775+SaenthuranVigna@users.noreply.github.com>
2021-11-14 14:51:58 -0500

    added comments in main

--- restaraunt_app/lib/main.dart
+++ restaraunt_app/lib/main.dart
@@ -81,6 +81,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
           child: Column(
+            //Buttons for Login/Register
+            //When clicked users will be routed to their Login/Register page 
         children: [
           ElevatedButton(
               onPressed: () {
