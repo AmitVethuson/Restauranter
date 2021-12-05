7305109fe9e053c566b6dbfb3d895f7c948bb6d4
VethusonAmit <vethuson.amit@ontariotechu.net>
2021-12-05 16:30:14 -0500

    Comments were added to menu_page"

--- restaraunt_app/lib/menu_page.dart
+++ restaraunt_app/lib/menu_page.dart
@@ -4,7 +4,7 @@ class MenuPage extends StatefulWidget {
   @override
   _MenuPageState createState() => _MenuPageState();
 }
-
+//Menu Pages
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
@@ -12,6 +12,7 @@ class _MenuPageState extends State<MenuPage> {
       appBar: AppBar(
         title: const Text("Menu"),
       ),
+      //listview of menu items
       body: ListView(
         children: [
           Container(
@@ -21,6 +22,7 @@ class _MenuPageState extends State<MenuPage> {
                     color: Colors.red,
                     fontSize: 25,
                   ))),
+          // add starter menu items to listview
           createContainer(
               "Garlic Escargot",
               "Topped with mozzarella and parmesan served with garlic toast.",
@@ -39,6 +41,7 @@ class _MenuPageState extends State<MenuPage> {
                     color: Colors.red,
                     fontSize: 25,
                   ))),
+          // add entrees menu items to listview
           createContainer("Danish Baby Back Pork Rum Ribs - Full",
               "Delicious with thick rum Sauce.", "\$23.99"),
           createContainer("Lemon Chicken",
@@ -56,6 +59,7 @@ class _MenuPageState extends State<MenuPage> {
                     color: Colors.red,
                     fontSize: 25,
                   ))),
+          // add Other Choices menu items to listview
           createContainer("Steak Sandwhich - 8oz",
               "Served on garlic toast with house salad and fries.", "\$19.99"),
           createContainer("Fish 'n Chips",
@@ -72,6 +76,7 @@ class _MenuPageState extends State<MenuPage> {
   }
 }
 
+//MenuItem Containter Tile
 Widget createContainer(String menuItem, String menuDescprition, String price) {
   return ListTile(
     title: Text(menuItem),
